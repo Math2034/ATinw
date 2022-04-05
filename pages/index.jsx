12c533eb83f680956ba994c9a6740d78efa18ed9
Matheus R <61693870+Math2034@users.noreply.github.com>
2022-04-05 13:20:14 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Tudo(){
         <Topo />
         <Home titulo="Os Objetivos de Desenvolvimento Sustentável no Brasil" texto="Os Objetivos de Desenvolvimento Sustentável são um apelo global à ação para acabar com a pobreza, proteger o meio ambiente e o clima e garantir que as pessoas, em todos os lugares, possam desfrutar de paz e de prosperidade. Estes são os objetivos para os quais as Nações Unidas estão contribuindo a fim de que possamos atingir a Agenda 2030 no Brasil."/>
         
-        <Intro titulo="Objetivo 6 " texto="Erradicar a fome, alcançar a segurança alimentar, melhorar a nutrição e promover a agricultura sustentável"/>
+        <Intro titulo="Objetivo 11 " texto="Cidades e comunidades sustentáveis"/>
         <Metas />
         </>
     )
